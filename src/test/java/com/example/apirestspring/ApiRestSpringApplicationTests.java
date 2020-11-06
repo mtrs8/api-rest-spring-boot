a2436b45f366f1b215f8e0ebc61192dabcf1e49f
Matheus Alves <mr634580@gmail.com>
2020-11-06 10:22:41 -0300

    Initial commit using spring

---
+++ src/test/java/com/example/apirestspring/ApiRestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
