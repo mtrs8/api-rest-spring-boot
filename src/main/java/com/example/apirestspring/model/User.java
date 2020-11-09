64fcec385e2612fd3c16e91d0882e7f48b5a4d72
Matheus Alves <mr634580@gmail.com>
2020-11-09 09:41:52 -0300

    config h2 database usando jpa

--- src/main/java/com/example/apirestspring/model/User.java
+++ src/main/java/com/example/apirestspring/model/User.java
@@ -1,10 +1,14 @@
 package com.example.apirestspring.model;
 
 import java.io.Serializable;
+import javax.persistence.*;
 
+@Entity
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String email;
